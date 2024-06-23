7a996346105a4a6d7f927add7d6d05bfe3bad58f
Pankaj Kumar <136339307+Pankajkumar34@users.noreply.github.com>
2024-06-23 17:10:47 +0530

    add fa bars

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import logo from '../assets/logo/plogo.png';
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
     const pathname = usePathname()
     return (
@@ -11,9 +13,9 @@ const Navbar = () => {
             <div className='container'>
                 <div className='main_header1 flex justify-between items-center'>
                     <div className='logo'>
-<Image src={logo} className='w-[100px] '/>
+                        <Image src={logo} className='w-[100px] ' />
                     </div>
-                    <div className='menus flex items-center'>
+                    <div className='menus sm:hidden lg:block flex items-center'>
                         <ul className='flex gap-1 p-3 '>
                             <li className={` ${pathname === '/' ? 'active' : ''} cursor-pointer px-[40px]`}><Link href="/">Home</Link></li>
                             <li className='cursor-pointer px-[40px]'><Link href="/about">About</Link></li>
@@ -25,7 +27,10 @@ const Navbar = () => {
                             <h2>Phone</h2>
                         </div>
                     </div>
-                    
+                    <div className='relative  z-[99999] '>
+                     
+                        <FontAwesomeIcon icon={faBars} className='text-[30px]' />
+                    </div>
 
                 </div>
 
